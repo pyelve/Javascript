f300313a7128a74caae53d2bb55066399bdb0325
pyelve <68893468+pyelve@users.noreply.github.com>
2023-09-14 08:46:55 +0530

    Add files via upload

---
+++ 01-Console/script.js
@@ -0,0 +1,25 @@
+console.log(100);
+
+console.log("Welcome to Devbhumi!");
+
+console.log(12 , "Mahadev" , true);
+
+const x = 404;
+
+console.log(x);
+
+console.error("Alert to yamraj!");
+
+console.warn("Please be Humanbeing");
+
+console.table({name:"Nandi Maharaj" , email:"nandi@gmail.com"});
+
+console.group("Chetavni");
+console.log(x);
+console.error("Alert to yamraj!");
+console.warn("Please be Humanbeing");
+console.groupEnd();
+
+const style = 'padding: 10px; background-color:black; color:white';
+
+console.log("%cNamaste",style);
