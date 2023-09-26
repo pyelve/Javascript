c950736493187ac5ab8ae9bf79d03baced3f3b4f
pyelve <68893468+pyelve@users.noreply.github.com>
2023-09-26 10:57:59 +0530

    Add files via upload

---
+++ 07-Tyoe-Coercion/script.js
@@ -0,0 +1,23 @@
+// Type Coercion
+let x;
+
+x = 5 + '5';
+console.log(x , typeof x);
+
+x = 5 + Number('5');
+console.log(x , typeof x);
+
+x = 5 * '5';
+console.log(x , typeof x);
+
+x = 5 + null;
+console.log(x , typeof x);
+
+x = 5 + true;
+console.log(x , typeof x);
+
+x = 5 + false;
+console.log(x , typeof x);
+
+x = 5 + undefined;
+console.log(x , typeof x);
