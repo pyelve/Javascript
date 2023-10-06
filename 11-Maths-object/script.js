d45f5479b0e03cdc171d0c995d0c883e57a161a4
pyelve <68893468+pyelve@users.noreply.github.com>
2023-10-06 08:53:35 +0530

    Add files via upload

---
+++ 11-Maths-object/script.js
@@ -0,0 +1,31 @@
+let x;
+
+x = Math.sqrt(625);
+console.log(x);
+
+x = Math.abs(-35);
+console.log(x);
+
+x = Math.round(4.6);
+console.log(x);
+
+x = Math.ceil(4.3);
+console.log(x);
+
+x = Math.floor(4.7);
+console.log(x);
+
+x = Math.pow(4,3);
+console.log(x);
+
+x = Math.min(2,7,4,1);
+console.log(x);
+
+x = Math.max(3, 4.5, 6 , 6.5);
+console.log(x);
+
+x = Math.random();
+console.log(x);
+
+x = Math.round((Math.random() * 100));
+console.log(x);
